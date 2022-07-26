cb592a542eb1f819ca15b587075171bea8505d59
ahzi8100 <uzif8100@students.amikoma.ac.id>
2022-07-26 10:54:12 +0700

    travelapp

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('pages.admin.dashboard');
+    }
+}
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="section-footer mt-5 mb-4 border-top">
+    <div class="containter pt-5 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12 col-lg-3">
+                        <h3>FEATURES</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Review</a></li>
+                            <li><a href="#">Community</a></li>
+                            <li><a href="#">Social Media Kit</a></li>
+                            <li><a href="#">Affiliate</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <h3>ACCOUNT</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Refund</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Reward</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <h3>COMPANY</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Career</a></li>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Media</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <h3>GET CONNECTED</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Jakarta</a></li>
+                            <li><a href="#">Indonesia</a></li>
+                            <li><a href="#">0821 - 2222 - 8888</a></li>
+                            <li><a href="#">support@nomads.id</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row border-top justify-content-center align-item-center pt-4">
+            <div class="col-auto text-gray-500 font-weight-light">
+                2019 Copyright Nomads • All rights reserved • Made in Jakarta
+            </div>
+        </div>
+    </div>
+</footer>
